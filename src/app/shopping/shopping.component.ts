27cad64391b0bd23841761deba1b05adba420996
madhumitha13579 <madhumitha.p@muraai.com>
2022-12-08 17:16:50 +0530

    second change

--- src/app/shopping/shopping.component.ts
+++ src/app/shopping/shopping.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { SampleserviceService } from '../sampleservice.service';
 
 @Component({
@@ -11,10 +11,28 @@ import { SampleserviceService } from '../sampleservice.service';
 export class ShoppingComponent implements OnInit {
 
   cards: any
+  items:any
   constructor(private serv: SampleserviceService, private router: Router) { }
   ngOnInit(): void {
-    this.serv.getProductDetails().subscribe(d => {
-      this.cards = d
-    })
+    console.log(';;;;;;;;;;;;',this.router.url)
+    if(this.router.url.includes("shopping")){
+      this.serv.getProductDetails().pipe(map(((details:any)=>{
+        return details.filter(((key:any)=>key.id<7))
+      }))).subscribe(clothes => {
+        this.cards = clothes
+      })
+      
+    }else {
+      this.serv.getProductDetails().pipe(map(((details:any)=>{
+        return details.filter(((key:any)=>key.id>=7))
+      }))).subscribe(groceryItem => {
+        this.items = groceryItem
+      })
+    }
   }
 }
+
+// this.dataSource = this.serv.dataEvent$.pipe(map((n: any) => {
+//   return n.filter((x: any) => x.Wish)
+
+// }))
